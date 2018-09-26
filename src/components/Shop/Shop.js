f6021385e5017724d62f69f0e00b4e84c8a3ab0a
cristian G <cristiang@cristians-MacBook-Pro.local>
2018-09-25 23:08:09 -0500

    changed env

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import styled from "styled-components";
+
+// const MainWrapper = styled.section`
+
+//   display: flex;
+
+// `;
+
+export default class Shop extends Component {
+  render() {
+    return <div className="App" />;
+  }
+}
